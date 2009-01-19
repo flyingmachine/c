3e3007fa44c7dbe3e8dd6f96b622cc21f7bbea5f
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2009-01-18 21:51:33 -0500

    first temperature version

--- temperature.c
+++ temperature.c
@@ -4,6 +4,17 @@
   for fahr=0, 20, ..., 300 */
 main()
 {
-  printf("hello, world\n");
-  printf("what what\t");
+  int fahr, celsius;
+  int lower, upper, step;
+  
+  lower = 0;
+  upper = 300;
+  step = 20;
+  
+  fahr = lower;
+  while(fahr <= upper) {
+    celsius = 5 * (fahr-32) / 9;
+    printf("%3d\t%6d\n", fahr, celsius);
+    fahr = fahr + step;
+  }
 }
