f25922bee408735129675ba828f3a20a30df2465
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2011-08-20 10:11:42 -0400

    exercise 2

---
+++ cprogmod/ch3/ex2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+main()
+{
+  int month, day, year, item_number;
+  float price;
+  
+  printf("Enter item number: ");
+  scanf("%d", &item_number);
+  
+  printf("Enter unit price: ");
+  scanf("%f", &price);
+  
+  printf("Enter a date (mm/dd/yyyy): ");
+  scanf("%d/%d/%d", &month, &day, &year);
+  
+  
+  printf("Item\t\tUnit\t\tPurchase\n");
+  printf("\t\tPrice\t\tDate\n");
+  printf("%d\t\t$%4.2f\t\t%.2d/%.2d/%.4d\n", item_number, price, month, day, year);
+}
