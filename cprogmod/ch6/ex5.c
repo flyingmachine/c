2e0cd613190be4fa99f632b114c6f1df923009e0
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2011-08-22 20:03:24 -0400

    reversal of number of any length exercise

---
+++ cprogmod/ch6/ex5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+main()
+{
+  int to_reverse, chopped, power = 0;
+
+  printf("Enter a number: ");
+  scanf("%d", &to_reverse);
+  
+  printf("Reversed: ");
+  
+  do {
+    chopped = to_reverse % 10;
+    to_reverse = to_reverse / 10;
+    
+    printf("%d", chopped);
+    
+  } while(to_reverse > 0);
+  
+  printf("\n");
+}
