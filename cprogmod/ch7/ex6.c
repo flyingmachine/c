b14048310a77fd415d3465667fec529375a3bb61
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2011-08-27 08:20:25 -0400

    sizeof

---
+++ cprogmod/ch7/ex6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+main()
+{
+  // short s = 0;
+  // int i= 0;
+  // long l= 0;
+  // float f= 0;
+  // double d= 0;
+  // long double ld= 0;
+  
+  printf("sizeof short: %zu\n", sizeof(short));
+  printf("sizeof int: %zu\n", sizeof(int));
+  printf("sizeof long: %zu\n", sizeof(long));
+  printf("sizeof float: %zu\n", sizeof(float));
+  printf("sizeof double: %zu\n", sizeof(double));
+  printf("sizeof long double: %zu\n", sizeof(long double));
+}
